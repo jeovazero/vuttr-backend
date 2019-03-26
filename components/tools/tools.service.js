7a8305682c59600ca29a74c56b69bc9e3d0a5e40
jeovazero <jeovapereira2000@live.com>
2019-03-25 23:15:37 -0300

    feat(tools): update service

--- components/tools/tools.service.js
+++ components/tools/tools.service.js
@@ -1,34 +1,47 @@
 const KoaRouter = require('koa-router')
 const mongoose = require('mongoose')
-const Tool = mongoose.model('Tool')
+const User = mongoose.model('User')
 const router = new KoaRouter()
 const Joi = require('joi')
 const { ToolPayloadSchema } = require('./tools.schema.js')
 
-router.get('/', async ctx => {
-  const tag = ctx.query.tag || ''
-  const tools = await Tool.find({}, { _id: 0, __v: 0, owner: 0 }).sort({
-    id: 1
-  })
+const IS_TEST = process.env.NODE_ENV === 'test'
 
-  if (tag.length > 0) {
-    ctx.body = tools.filter(el => {
-      return el.tags.includes(tag)
-    })
+const getEmail = ctx => {
+  if (IS_TEST) {
+    return 'joaoarms@mail.com'
   } else {
-    ctx.body = tools
+    return ctx.state.user.email
+  }
+}
+
+router.get('/', async ctx => {
+  try {
+    const tag = ctx.query.tag || ''
+    const email = getEmail(ctx)
+    const tools = await User.findByEmailAndGetTools(email)
+
+    if (tag.length > 0) {
+      ctx.body = tools.filter(el => {
+        return el.tags.includes(tag)
+      })
+    } else {
+      ctx.body = tools
+    }
+  } catch (error) {
+    ctx.throw(500, 'Internal Server Error')
   }
 })
 
 router.post('/', async ctx => {
   try {
     const tool = ctx.request.body
+    const email = getEmail(ctx)
 
     const { error } = Joi.validate(tool, ToolPayloadSchema, { convert: false })
     if (error) throw Error(error.details[0].message)
 
-    const newTool = new Tool(tool)
-    const savedTool = await newTool.save()
+    const savedTool = await User.findByEmailAndAddTool(email, tool)
     ctx.body = {
       id: savedTool.id,
       title: savedTool.title,
@@ -44,8 +57,10 @@ router.post('/', async ctx => {
 router.delete('/:id', async ctx => {
   try {
     const id = ctx.params.id || 0
-    const { deletedCount } = await Tool.deleteOne({ id })
-    if (deletedCount === 0) throw new Error('Tool not found')
+    const email = getEmail(ctx)
+
+    const success = await User.findByEmailAndRemoveTool(email, id)
+    if (!success) throw new Error('Tool not found')
     ctx.body = {}
   } catch (error) {
     ctx.throw(404, error.message)
